a13de08a9d988159e7af29a75530d83bad8feedd
youloveli <mymhj122@gmail.com>
2024-03-07 15:34:13 +0900

    :lipstick: Dependency.kt

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,29 @@
+object Dependency {
+
+
+    object Compose {
+        const val ALPHA = "androidx.compose.material3:material3:${Versions.composeVersion}"
+        const val NAVIGATION = "androidx.navigation:navigation-compose:${Versions.navigationVersion}"
+        const val UI = "androidx.compose.ui:ui"
+        const val GRPAHICS = "androidx.compose.ui:ui-graphics"
+        const val MATERAIL = "androidx.compose.material3:material3"
+        const val ACTIVITY = "androidx.activity:activity-compose:${Versions.activityVersion}"
+        const val BOM = "androidx.compose:compose-bom:${Versions.bomVersion}"
+        const val PREVIEW = "androidx.compose.ui:ui-tooling-preview"
+        const val TOOLING = "androidx.compose.ui:ui-tooling"
+    }
+
+    object Ktx {
+        const val RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleVersion}"
+        const val CORE = "androidx.core:core-ktx:${Versions.coreVersion}"
+    }
+
+    object Test {
+        const val EXTJUNIT = "androidx.test.ext:junit:${Versions.junitVersion}"
+        const val junit = "junit:junit:${Versions.junitVersion}"
+        const val CORE = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+        const val UIJUNIT = "androidx.compose.ui:ui-test-junit4"
+        const val MANIFEST = "androidx.compose.ui:ui-test-manifest"
+    }
+
+}
