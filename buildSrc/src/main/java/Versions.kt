14baf3316f8be405eb4cb1dd72442f33f6f290fb
youloveli <mymhj122@gmail.com>
2024-03-07 15:33:27 +0900

    :lipstick: Versions.kt

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val composeVersion = "1.3.0-alpha01"
+    const val navigationVersion = "2.7.7"
+    const val bomVersion = "2023.03.00"
+    const val activityVersion = "1.8.2"
+
+    const val lifecycleVersion = "2.7.0"
+    const val coreVersion = "1.12.0"
+
+    const val junitVersion = "4.13.2"
+    const val espressoVersion = "3.5.1"
+
+}
