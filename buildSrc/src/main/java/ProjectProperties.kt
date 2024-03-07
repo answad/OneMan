2b89c0d7584d7bd31af3576bff327e519593b678
youloveli <mymhj122@gmail.com>
2024-03-07 15:33:41 +0900

    :lipstick: ProjectProperties.kt

---
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -0,0 +1,9 @@
+object ProjectProperties {
+    const val compileSdk = 34
+    const val minSdk = 33
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val jvmTarget = "1.8"
+    const val kotlinCompilerExtensionVersion = "1.4.3"
+}
