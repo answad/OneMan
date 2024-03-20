b955b37209cffb76b70768e368552969d863f12c
혜성 <118062596+answad@users.noreply.github.com>
2024-03-21 08:26:04 +0900

    Update Sun.kt

--- app/src/main/java/com/chobo/oneman/Sun.kt
+++ app/src/main/java/com/chobo/oneman/Sun.kt
@@ -1,2 +1,24 @@
 package com.chobo.oneman
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+const val SunRoute = "챗봇"
+
+fun NavController.navigateToSun() {
+    this.navigate(SunRoute)
+}
+
+fun NavGraphBuilder.sun(
+    paddingValues: PaddingValues
+) {
+    composable(SunRoute) {
+        Spacer(modifier = Modifier.padding(top = paddingValues.calculateTopPadding()))
+        TopAppBar(SunRoute)
+    }
+}
