f536a04fd988af4044a0d7f1239430cdb0b0d89d
혜성 <118062596+answad@users.noreply.github.com>
2024-03-21 08:25:51 +0900

    Update People.kt

--- app/src/main/java/com/chobo/oneman/People.kt
+++ app/src/main/java/com/chobo/oneman/People.kt
@@ -1,2 +1,25 @@
 package com.chobo.oneman
 
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+const val myPageRoute = "챗봇"
+
+fun NavController.navigateToMyPage() {
+    this.navigate(myPageRoute)
+}
+
+fun NavGraphBuilder.myPage(
+    paddingValues: PaddingValues
+) {
+    composable(myPageRoute) {
+        Spacer(modifier = Modifier.padding(top = paddingValues.calculateTopPadding()))
+        TopAppBar(myPageRoute)
+    }
+}
