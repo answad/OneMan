08ba4da70e57f9ce471bd34ad66f93af29f7d172
혜성 <118062596+answad@users.noreply.github.com>
2024-03-21 08:24:25 +0900

    Update ChatBot.kt

--- app/src/main/java/com/chobo/oneman/ChatBot.kt
+++ app/src/main/java/com/chobo/oneman/ChatBot.kt
@@ -1,3 +1,24 @@
 package com.chobo.oneman
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 
+const val chatBotRoute = "챗봇"
+
+fun NavController.navigateToChatBot() {
+    this.navigate(chatBotRoute)
+}
+
+fun NavGraphBuilder.chatBot(
+    paddingValues: PaddingValues
+) {
+    composable(chatBotRoute) {
+        Spacer(modifier = Modifier.padding(top = paddingValues.calculateTopPadding()))
+        TopAppBar(chatBotRoute)
+    }
+}
